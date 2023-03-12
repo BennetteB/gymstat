73b3753b5eeedb29118a4c05f484fbeb9ab9c7f9
Bennette Benoy <bennette.benoy@outlook.com>
2023-03-12 20:47:59 +1000

    add dashboard to router

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Landing from './pages/Landing'
+import Dashboard from './pages/Dashboard'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Landing />
   },
+  {
+    path: '/dashboard',
+    element: <Dashboard />
+  }
 ])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
